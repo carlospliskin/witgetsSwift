9ebd1423790efde9370efe1600043fcdf64d30e0
Carlos Enrique Paredes <carlospliskin21@gmail.com>
2021-08-03 09:10:28 -0500

    Initial Commit

---
+++ nuevo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nuevo
+//
+//  Created by carlos paredes on 03/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
